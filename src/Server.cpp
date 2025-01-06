d50ef2e7ec8f6707a4860fa03cf71d41358dd46e
andy <acbeers1@gmail.com>
2025-01-05 21:32:44 -0500

    codecrafters submit [skip ci]

--- src/Server.cpp
+++ src/Server.cpp
@@ -3,13 +3,40 @@
 #include <unordered_set>
 
 bool match_pattern(const std::string& input_line, const std::string& pattern) {
-    std::unordered_set<char> digits({'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'});
+    const std::unordered_set<char> digits({'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'});
+    const std::unordered_set<char> uppercaseLetters({'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+                                               'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+                                               'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'});
+    const std::unordered_set<char> lowercaseLetters({'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+                                               'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+                                               'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'});
     if (pattern.length() == 1) {
         return input_line.find(pattern) != std::string::npos;
     }
     else if (pattern == "\\d"){
         for(char curr : input_line){
             if (digits.find(curr) != digits.end()){
+                return true;
+            }
+        }
+
+        return false;
+    }
+    else if (pattern == "\\w"){
+        for(char curr : input_line){
+            if (digits.find(curr) != digits.end()){
+                return true;
+            }
+
+            if (uppercaseLetters.find(curr) != uppercaseLetters.end()){
+                return true;
+            }
+
+            if (lowercaseLetters.find(curr) != lowercaseLetters.end()){
+                return true;
+            }
+
+            if (curr == '_'){
                 return true;
             }
         }
